8117a9cee7a62b303b87c77db36235785c346c6a
Pablo Herrero <pablodherrero@gmail.com>
2012-04-20 10:36:05 -0300

    Add some test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from datetime import datetime
+
 from tp import *
 
 class UserTests(unittest.TestCase):
@@ -7,6 +9,19 @@ class UserTests(unittest.TestCase):
         user = User("Pablo", "pablo@pablo.com")
         self.assertEqual("Pablo", user.name)
         self.assertEqual("pablo@pablo.com", user.email)
+
+class JourneyStopTests(unittest.TestCase):
+    def setUp(self):
+        self.place = Place()
+        self.occurrence = SingleTimeOccurrence(datetime.now)
+        self.passenger = User("Pablo", "pablo@pablo.com")
+
+    def test_init(self):
+        stop = JourneyStop(self.place, self.occurrence, self.passenger)
+        self.assertEqual(self.place, stop.place)
+        self.assertEqual(self.occurrence, stop.occurrence)
+        self.assertEqual(self.passenger, stop.passenger)
+
 
 if __name__ == "__main__":
     unittest.main()

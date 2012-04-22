310ae8cbae26581ddec3d2ff956402c53f60fa2a
Pablo Herrero <pablodherrero@gmail.com>
2012-04-22 05:51:45 -0300

    Many refactorings

--- tp.py
+++ tp.py
@@ -30,63 +30,61 @@ class DayOfWeek:
     def __lt__(self, other_week_day):
         return self.ordinal < other_week_day.ordinal
 
-    def same_day(self, adatetime):
+    def same_day_as(self, adatetime):
         return adatetime.weekday() == self.ordinal
 
 DayOfWeek.days_of_week = tuple(DayOfWeek(name, number) for number, name in
     enumerate(DayOfWeek.day_names))
 
 
-class Temporality:
-    def is_happening_at(self, date_time):
+class Timetable: # Or Schedule (both are more or less synonyms)
+    def is_happening_at(self, adatetime):
         raise NotImplementedError()
 
+    def is_during_this_date(self, adate):
+        raise NotImplementedError()
 
-class SingleTimeTemporality(Temporality):
+class SingleTimeTimetable(Timetable):
     def __init__(self, datetime):
         self.daytime = datetime
 
     def is_happening_at(self, adatetime):
         return self.datetime == adatetime
 
-class RepetitiveTemporality(Temporality):
+    def is_during_this_date(self, adate):
+        return self.datetime.date() == adate
+
+class RepetitiveTimetable(Timetable):
     pass
 
-class WeeklyTemporality(RepetitiveTemporality):
-    def __init__(self, time, days_of_week):
+class WeeklyTimetable(RepetitiveTimetable):
+    def __init__(self, atime, days_of_week):
         self.days_of_week = days_of_week
-        self.time = time
+        self.time = atime
 
     def is_happening_at(self, adatetime):
-        return any(day_of_week.same_day(adatetime) for day_of_week in self.days_of_week)
+        return any(day_of_week.same_day_as(adatetime) and datetime.time() == self.time for day_of_week in self.days_of_week)
 
-
-class ScheduledEvent:
-    def __init__(self, place, temporality):
-        self.place = place
-        self.temporality = temporality
-
-    def takes_place_at(self, place):
-        return self.place == place
-
-    def is_happening_at(self, adatetime):
-        return self.temporality.is_happening_at(adatetime)
+    def is_during_this_date(self, adatetime):
+        return any(day_of_week.same_day_as(adatetime) for day_of_week in self.days_of_week)
 
 
 class JourneyRequest:
     #TODO: ¿La vuelta del trabajo, importa?
-    def __init__(self, passenger, departure, arrival):
+    def __init__(self, passenger, origin, destination, timetable):
         self.passenger = passenger
-        self.departure = departure
-        self.arrival = arrival
+        self.origin = origin
+        self.destination = destination
+        self.timetable = timetable
 
 
 class JourneyOffer:#TODO: TransportOffer?
-    def __init__(self, driver, departure, arrival, passenger_capacity):
+    def __init__(self, driver, origin, destination, timetable, passenger_capacity):
         self.driver = driver
+        self.origin = origin
+        self.destination = destination
+        self.timetable = timetable
         self.passengers_capacity = passengers_capacity
-        self.departure = departure
-        self.arrival = arrival
 
 
 class Journey:
@@ -118,8 +116,8 @@ class JourneyOrganizer:
         self.requests = filter_by_date(requests)
         self.offers = filter_by_date(offers)
 
-    def filter_by_date(self, events):
-        return [event for event in events if event.ocurrence.is_happening_at(self.date)]
+    def filter_by_date(self, schedulables):
+        return [schedulable for schedulable in schedulables if event.timetable.is_happening_at(self.date)]
 
     def organize(self):
         pass

3a9047a5f99d6ab4ef85f090b0e0cb4b7acdbd46
Pablo Herrero <pablodherrero@gmail.com>
2012-05-20 23:14:31 -0300

    lots and lots of changes

--- test.py
+++ test.py
@@ -1,63 +1,63 @@
-    import unittest
+import unittest
 
-    import datetime
+import datetime
 
-    from tp import *
+from tp import *
 
-    class UserTests(unittest.TestCase):
-        def test_init(self):
-            user = User("Pablo", "pablo@pablo.com")
+class UserTests(unittest.TestCase):
+    def test_init(self):
+        user = User("Pablo", "pablo@pablo.com")
 
-            self.assertEqual("Pablo", user.name)
-            self.assertEqual("pablo@pablo.com", user.email)
+        self.assertEqual("Pablo", user.name)
+        self.assertEqual("pablo@pablo.com", user.email)
 
-    class DayOfWeekTests(unittest.TestCase):
-        def test_init(self):
-            lunes = DayOfWeek("Lunes", 0)
+class DayOfWeekTests(unittest.TestCase):
+    def test_init(self):
+        lunes = DayOfWeek("Lunes", 0)
 
-            self.assertEqual("Lunes", lunes.name)
-            self.assertEqual(0, lunes.ordinal)
+        self.assertEqual("Lunes", lunes.name)
+        self.assertEqual(0, lunes.ordinal)
 
-        def test_lt(self):
-            lunes = DayOfWeek("Lunes", 0)
-            martes = DayOfWeek("Martes", 1)
+    def test_lt(self):
+        lunes = DayOfWeek("Lunes", 0)
+        martes = DayOfWeek("Martes", 1)
 
-            self.assertTrue(lunes < martes)
+        self.assertTrue(lunes < martes)
 
-    class JourneyStopTests(unittest.TestCase):
-        def setUp(self):
-            self.where = Place()
-            self.when = datetime.now
-            self.passengers = [User("Pablo", "pablo@pablo.com")]
+class JourneyStopTests(unittest.TestCase):
+    def setUp(self):
+        self.where = Place()
+        self.when = datetime.now
+        self.passengers = [User("Pablo", "pablo@pablo.com")]
 
-    class JourneyStopForBoardingTests(JourneyStopTests):
-        def test_init(self):
-            stop_for_boarding = JourneyStopForBoarding(self.where, self.when, self.passengers)
+class JourneyStopForBoardingTests(JourneyStopTests):
+    def test_init(self):
+        stop_for_boarding = JourneyStopForBoarding(self.where, self.when, self.passengers)
 
-            self.assertEqual(self.where, stop_for_boarding.place)
-            self.assertEqual(self.when, stop_for_boarding.datetime)
-            self.assertEqual(self.passengers, stop_for_boarding.passengers)
+        self.assertEqual(self.where, stop_for_boarding.place)
+        self.assertEqual(self.when, stop_for_boarding.datetime)
+        self.assertEqual(self.passengers, stop_for_boarding.passengers)
 
-    class JourneyStopForDischargingTests(JourneyStopTests):
-        def test_init(self):
-            stop_for_discharging = JourneyStopForDischarging(self.where, self.when, self.passengers)
+class JourneyStopForDischargingTests(JourneyStopTests):
+    def test_init(self):
+        stop_for_discharging = JourneyStopForDischarging(self.where, self.when, self.passengers)
 
-            self.assertEqual(self.where, stop_for_discharging.place)
-            self.assertEqual(self.when, stop_for_discharging.datetime)
-            self.assertEqual(self.passengers, stop_for_discharging.passengers)
+        self.assertEqual(self.where, stop_for_discharging.place)
+        self.assertEqual(self.when, stop_for_discharging.datetime)
+        self.assertEqual(self.passengers, stop_for_discharging.passengers)
 
-    class CommuterRegistrationTest(unittest.TestCase):
-        def setUp(self):
-            self.user = User("Pablo", "pablo@pablo.com")
+class CommuterRegistrationTest(unittest.TestCase):
+    def setUp(self):
+        self.user = User("Pablo", "pablo@pablo.com")
 
 
-        def test_register_a_request_for_commuting(self):
-            """
-            Como oficinista quiero registrar que
-            todos los Lunes, Martes y Miercoles, salgo de mi casa a las 8:00 AM y tengo que llegar a mi trabajo a las 9:00 AM
-            y los Jueves y Viernes salgo de la casa de mi novia a las 8:30 AM y tengo que llegar a mi trabajo a las 9:30 AM
+    def test_register_a_request_for_commuting(self):
         """
-        pass
+        Como oficinista quiero registrar que
+        todos los Lunes, Martes y Miercoles, salgo de mi casa a las 8:00 AM y tengo que llegar a mi trabajo a las 9:00 AM
+        y los Jueves y Viernes salgo de la casa de mi novia a las 8:30 AM y tengo que llegar a mi trabajo a las 9:30 AM
+    """
+    pass
 
     def test_register_a_request_for_commuting_with_a_car_offer(self):
         """
@@ -75,7 +75,7 @@ class JourneyOrganizerTest(unittest.TestCase):
             proponent = User("Pablo", "pablo@pablo.com")
             address1 = Address()
             address2 = Address()
-            timetable = WeeklyTimetable(datetime.time(8, 30), (MONDAY, WEDNESDAY)) 
+            timetable = WeeklyTimetable(datetime.time(8, 30), (MONDAY,)) 
             return JourneyProposalWithVehicule(proponent, address1, address2, 
                 timetable, 2)
 
@@ -83,19 +83,51 @@ class JourneyOrganizerTest(unittest.TestCase):
             proponent = User("Rodrigo", "rodrigo@rodrigo.com")
             address1 = Address()
             address2 = Address()
-            timetable = WeeklyTimetable(datetime.time(8, 30), (MONDAY, WEDNESDAY)) 
+            timetable = WeeklyTimetable(datetime.time(8, 25), (MONDAY,)) 
             return JourneyProposalWithoutVehicule(proponent, address1, address2, 
                 timetable)
 
-        self.proposal1 = proposal_with_car() 
-        self.proposal2 = proposal_without_car() 
+        self.proposal_with_car = proposal_with_car() 
+        self.proposal_without_car = proposal_without_car() 
         self.timedelta = datetime.timedelta(minutes=15)
+        self.aweek_interval = DateTimeInterval(datetime.datetime(2012, 5, 14), datetime.datetime(2012, 5, 19))
+        self.distance_tolerance = 10
+
+        self.organizer = JourneyOrganizer([self.proposal_with_car, self.proposal_without_car],
+            self.aweek_interval, self.timedelta, self.distance_tolerance)
 
 
     def test_organizer_should_create_journey_for_compatible_proposals(self):
-        JourneyOrganizer(self.timedelta, [self.proposal1, self.proposal2])
+        journeys = self.organizer.organize()
 
-    def __init__(self, date, tolerance, requests, offers):
+        assertEqual(1, len(journeys))
+
+        journey = journeys[0]
+        assertEqual(self.proposal_with_car, journey.accepted_proposal)
+        assertEqual(2, len(journeys.stops))
+
+        stops = journeys.stops
+        assertEqual(self.proposal_with_car.origin, stops[0].place)
+        assertEqual(self.proposal_with_car.destination, stops[1].place)
+
+        assertTrue(self.proposal_without_car.origin.is_near(stops[0].place, self.distance_tolerance))
+        assertTrue(self.proposal_without_car.destination.in_near(stops[1].place, self.distance_tolerance))
+
+        users = {self.proposal_with_car.proponent, self.proposal_without_car.proponent}
+        assertEqual(users, set(self.stops[0].passengers_stepping_in))
+        assertEqual(users, set(self.stops[1].passengers_leaving))
+        assertEqual(0, len(self.stops[0].passengers_leaving))
+        assertEqual(0, len(self.stops[1].passengers_stepping_in))
+
+        delta = self.stops[0].datetime - self.proposal_without_car.timetable.ocurrences()[0]
+        assertTrue(abs(delta.total_seconds()) <= self.timedelta.total_seconds())
+
+        delta = self.stops[0].datetime - self.proposal_with_car.timetable.ocurrences()[0]
+        assertTrue(abs(delta.total_seconds()) <= self.timedelta.total_seconds())
+
+        assertEqual(journeys.stops[0].datetime.date(), journey.date())
+
+        
 
 if __name__ == "__main__":
     unittest.main()
--- tp.py
+++ tp.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
 from datetime import datetime
+from functools import reduce
+from itertools import groupby, chain
 
 class User:
     """Represents a registered user."""
@@ -10,6 +12,9 @@ class User:
 
 
 class Place:
+    def is_near(self, place, tolerance):
+        raise self.distance_to(place) <= tolerance
+
     def distance_to(self, place):
         """Returns distance between two places in meters"""
         raise NotImplementedError()
@@ -41,9 +46,9 @@ FRIDAY    = DayOfWeek("Viernes")
 SATURDAY  = DayOfWeek("Sábado")
 SUNDAY    = DayOfWeek("Domingo")
 
-DayOfWeek.days_of_week = [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY] 
+DayOfWeek.days_of_week = (MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY)
 
-class TimeInterval:
+class DateTimeInterval:
     def __init__(self, begin, end):
         #TODO: Validacion: begin < end
         self._begin = begin
@@ -70,7 +75,7 @@ class Timetable: # Or Schedule (both are more or less synonyms)
     def happens_this_date(self, adate):
         raise NotImplementedError()
 
-    def ocurrences_for_interval(self, interval):
+    def ocurrences(self, interval):
         raise NotImplementedError()
 
     def times_within_inverval(self, interval):
@@ -84,7 +89,7 @@ class SingleTimeTimetable(Timetable):
     def __init__(self, datetime):
         self.daytime = datetime
 
-    def ocurrences_for_interval(self, interval):
+    def ocurrences(self, interval):
         if interval.overlaps(self.daytime):
             return [self.daytime]
         else:
@@ -98,14 +103,14 @@ class WeeklyTimetable(RepetitiveTimetable):
         self.days_of_week = days_of_week
         self.time = atime
 
-    def ocurrences_for_interval(self, interval):
+    def ocurrences(self, interval):
         timetable_datetimes = [datetime.combine(adate, self.time) for adate in interval.included_days()]
         
         def included_datetime(adatetime):
-            return interval.begin() <= adatetime < interval.end() and
+            return interval.begin() <= adatetime < interval.end() and \
                 any(weekday.same_day_as(adatime) for weekday in self.weekdays)
 
-        return filter(included_datetime, timetable_datetimes)
+        return list(filter(included_datetime, timetable_datetimes))
 
 
 class JourneyProposal:
@@ -115,7 +120,7 @@ class JourneyProposal:
         self.destination = destination
         self.timetable = timetable
 
-    def has_vehicule:
+    def has_vehicule():
         raise NotImplementedError()
 
     def plausible_offers(self, offers):
@@ -127,7 +132,7 @@ class JourneyProposalWithVehicule(JourneyProposal):
         super().__init__(proponent, origin, destination, timetable)
         self.passenger_capacity = passenger_capacity
 
-    def has_vehicule:
+    def has_vehicule():
         return True
 
     def satisfies(self, request):
@@ -138,37 +143,118 @@ class JourneyProposalWithVehicule(JourneyProposal):
 
 
 class JourneyProposalWithoutVehicule(JourneyProposal):
-    def has_vehicule:
+    def has_vehicule():
         return False
 
 
 class Journey:
-    def __init__(self, accepted_offer, date, stops):
+    def __init__(self, accepted_proposal, adatetime, stops):
         self.accepted_proposal = accepted_proposal
-        self.date = date
         self.stops = stops
+        self.datetime = adatetime
 
+    def people(self):
+        return reduce(set.union, { set(stop.passengers_stepping_in) for stop in self.stops })
 
+    def date(self):
+        return self.stops[0].datetime.date()
+
+    @classmethod
+    def create_journeys_for_proposal(cls, proposal, interval):
+        journeys = []
+        for adatetime in proposal.timetable.ocurrences(interval):
+            begin = JourneyStop(proposal.origin, [proposal.proponent], [])
+            end = JourneyStop(proposal.destination, [], [proposal.proponent])
+            journeys.append(Journey(proposal, adatetime, [begin, end]))
+
+        return journeys
+
+    def total_seats(self):
+        return accepted_proposal.capacity
+
+    def spare_seats(self):
+        return self.total_seats - len(self.people())
+
+    def has_spare_seats(self):
+        return self.spare_seats() > 0
+
+    def start_point(self):
+        return self.stops[0].place
+
+    def end_point(self):
+        return self.stops[-1].place
+
+    
 class JourneyStop:
-    def __init__(self, place, datetime, passengers_stepping_in, passengers_leaving):
+    def __init__(self, place, passengers_stepping_in, passengers_leaving):
         self.place = place
-        self.datetime = datetime
         self.passengers_stepping_in = passengers_stepping_in
         self.passengers_leaving = passengers_leaving
 
 
 class JourneyOrganizer:
-    def __init__(self, proposals, date_interval, time_tolerance, distance_tolerance):
+    def __init__(self, proposals, interval, time_tolerance, distance_tolerance):
         self.proposal = proposals
-        self.interval = date_interval
+        self.interval = interval
         self.time_tolerance = time_tolerance #Late tolerance as a lapse of time (some minutes/hours)
         self.distance_tolerance = distance_tolerance
+
+        self.proposals_with_vehicule = filter(lambda proposal: proposal.has_vehicule(), self.proposals)
+        self.proposals_without_vehicule = proposals - proposals_without_vehicule
+
+        self.results = []
+        self.left_overs = []
 
     def plausible_offers_for(self, request):
         return request.plausible_offers(self.offers)
 
     def organize(self):
-        pass
+        self.create_journeys_for_proposal_with_wehicules()
+        self.match_proposals_with_journeys()
+        self.optimize_results()
+
+    def create_journeys_for_proposal_with_wehicules(self):
+        def journeys_for(proposal):
+            return Journey.create_journeys_for_proposal(proposal, self.interval)
+
+        self.results = list(chain(*map(journeys_for, proposals_with_vehicule)))
+
+    def match_proposals_with_journeys(self):
+        for proposal in self.proposals_without_vehicule:
+            for adatetime in proposal.timetable.ocurrences(interval):
+                journeys = (candidate for candidate in self.results if self.are_compatible(candidate, proposal, adatetime) and candidate.has_spare_seats())
+                journeys.sort(key=Journey.spare_seats)
+                
+                if len(journey) > 0:
+                    journeys[0].add_passenger(proposal)
+                else:
+                    #FIX!!!!!!!!!!!!!!!
+                    left_overs.append((proposal,adatetime))
+
+    def optimize_results(self):
+        self.results.sort(key=lambda journey: len(journey.people()))
+        other_journeys = self.results[:]
+
+        for journey in self.results:
+            other_journeys.remove(journey)
+            other_journeys = list(filter(Journey.has_spare_seats, other_journeys))
+            other_journeys.sort(key=lambda journey: journey.spare_seats, reverse=True)
+            other_journeys.sort(key=lambda journey: journey.total_seats)
+
+            for other_journey in other_journeys:
+                journeys.move_passengers_to(other_journey)
+
+        self.results = [journey for journey in candidate_journeys if len(journey.people) > 0]
+
+
+    def are_compatible(self, journey, proposal, adatetime):
+        is_near = journey.start_point.is_near(proposal.origin, self.distance_tolerance) and \
+            journey.end_point.is_near(proposal.destination, self.distance_tolerance) 
+
+        is_close_in_time = abs((journey.datetime - adatetime).total_seconds()) <= self.time_tolerance.total_seconds()
+
+        return is_near and is_close_in_time
+
 
 class Notification:
     def content(self):
